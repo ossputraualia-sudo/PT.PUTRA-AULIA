9b8972e7df461b7336892eab65de2120490c63b4
ossputraualia-sudo <oss.putraualia@gmail.com>
2026-08-18 03:35:38 +0700

    Update 42_GPSPhoto.js

--- js/42_GPSPhoto.js
+++ js/42_GPSPhoto.js
@@ -1,1 +1,72 @@
-PAG.42_GPSPhoto={};
+PAG.42_GPSPhoto = {
+
+  async capturePosition() {
+
+    return await PAG.GPS.get();
+
+  },
+
+
+  async attach(photo) {
+
+    if (!photo) {
+
+      throw new Error(
+        "Data foto tidak ditemukan."
+      );
+
+    }
+
+
+    const gps =
+      photo.gps ||
+      await this.capturePosition();
+
+
+    return {
+
+      ...photo,
+
+      gps: {
+
+        lat:
+          gps.lat,
+
+        lng:
+          gps.lng,
+
+        accuracy:
+          gps.accuracy,
+
+        capturedAt:
+          gps.capturedAt ||
+          new Date().toISOString()
+
+      }
+
+    };
+
+  },
+
+
+  format(gps) {
+
+    if (!gps) {
+      return "";
+    }
+
+
+    return (
+      Number(gps.lat).toFixed(6) +
+      ", " +
+      Number(gps.lng).toFixed(6) +
+      " ± " +
+      Math.round(
+        Number(gps.accuracy || 0)
+      ) +
+      " m"
+    );
+
+  }
+
+};
