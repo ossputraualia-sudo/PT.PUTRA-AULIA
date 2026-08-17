62bcf446804e757f7bbadc978b9bc1af6f388571
ossputraualia-sudo <oss.putraualia@gmail.com>
2026-08-18 03:08:49 +0700

    Update 01_Config.js

--- js/core/01_Config.js
+++ js/core/01_Config.js
@@ -1,1 +1,31 @@
-window.PAG={};PAG.CONFIG={MAIN_WEBAPP_URL:'https://script.google.com/macros/s/AKfycbyQhAEsSsNrX60rMu6zc3hx_0mb4lJXnu-H_c5VfJvIzgbR1fdjV1VzF1kZVBZPamkA/exec',FIELD_BACKEND_URL:'https://script.google.com/macros/s/AKfycbzo6SzFdVWCDOc8pFvqIFBF-dwFFpkGJoscLYKR_zjcSUW1Hwcug9VKEtNwGbZ7w6OI-g/exec',DB_NAME:'PAG_DOCS_FIELD',DB_VERSION:1};
+window.PAG = window.PAG || {};
+
+PAG.CONFIG = {
+
+  // =====================================================
+  // WEB UTAMA
+  // =====================================================
+
+  MAIN_WEBAPP_URL:
+    "https://script.google.com/macros/s/AKfycbyQhAEsSsNrX60rMu6zc3hx_0mb4lJXnu-H_c5VfJvIzgbR1fdjV1VzF1kZVBZPamkA/exec",
+
+
+  // =====================================================
+  // FIELD BACKEND
+  // =====================================================
+
+  FIELD_BACKEND_URL:
+    "PASTE_FIELD_BACKEND_URL_HERE",
+
+
+  // =====================================================
+  // LOCAL DATABASE
+  // =====================================================
+
+  DB_NAME:
+    "PAG_DOCS_FIELD",
+
+  DB_VERSION:
+    1
+
+};
